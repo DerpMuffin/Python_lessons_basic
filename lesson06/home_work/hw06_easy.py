47293b1e71f0ec79002b693088ddc641df03aff1
Serega Abissov <abissovs@gmail.com>
2019-01-09 21:49:49 +0300

    Lesson 6 Homework is done (easy & normal & hard) + added files(with names) for task in hw06_normal.py

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,5 +1,74 @@
+class Vector:
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __str__(self):
+        return f'( {self.start}; {self.end} )'
+
+    def __get_legs(self):
+        k1 = abs(self.start[0] - self.end[0])
+        k2 = abs(self.start[1] - self.end[1])
+        return k1, k2
+
+    def get_length(self):
+        k1, k2 = self.__get_legs()
+        return (k1 ** 2 + k2 ** 2) ** (1 / 2)
+
+    def get_point(self):
+        return self.end[0] - self.start[0], self.end[1] - self.start[1]
+
+    def get_matrix(self):
+        return [[self.start[0], self.end[0]], [self.start[1], self.end[1]]]
+
+
+class Shape:
+
+    def __init__(self, points):
+        """
+        Base class shapes
+        :param points: List of tuples with coord X, Y
+        """
+        count = len(points)
+        self.vectors = []
+        for i in range(count):
+            self.vectors.append(Vector(points[i], points[((i + 1) % count)]))
+
+    def get_square(self):
+        vectors_det = []
+        for v in self.vectors:
+            m = v.get_matrix()
+            vectors_det.append(m[0][0] * m[1][1] - m[0][1] * m[1][0])
+        return abs(sum(vectors_det) / 2)
+
+    def get_perimeter(self):
+        return sum([v.get_length() for v in self.vectors])
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+
+
+class Triangle(Shape):
+
+    def __init__(self, a, b, c):
+        """
+        Triangle class
+        :param a: Tuple with coord X, Y
+        :param b: Tuple with coord X, Y
+        :param c: Tuple with coord X, Y
+        """
+        super().__init__([a, b, c])
+
+    def get_triangle_square(self):
+        pp = self.get_perimeter() / 2
+        a = self.vectors[0].get_length()
+        b = self.vectors[1].get_length()
+        c = self.vectors[2].get_length()
+        return (pp * (pp - a) * (pp - b) * (pp - c)) ** (1/2)
+
+    def get_height(self, vector_num=0):
+        return self.get_square() * 2 / self.vectors[vector_num].get_length()
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -7,3 +76,33 @@
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+
+class Trapeze(Shape):
+
+    def __init__(self, a, b, c, d):
+        """
+        Trapeze class
+        :param a: Tuple with coord X, Y
+        :param b: Tuple with coord X, Y
+        :param c: Tuple with coord X, Y
+        :param d: Tuple with coord X, Y
+        """
+        super().__init__([a, b, c, d])
+
+    def check_equilateral(self):
+
+        def __check_equal_diagonal(vector1, vector2):
+            d1 = Vector(vector1.start, vector2.start)
+            d2 = Vector(vector1.end, vector2.end)
+            return d1.get_length() == d2.get_length()
+
+        if self.vectors[0].get_length() == self.vectors[2].get_length():
+            return __check_equal_diagonal(self.vectors[0], self.vectors[2])
+        elif self.vectors[1].get_length() == self.vectors[3].get_length():
+            return __check_equal_diagonal(self.vectors[1], self.vectors[3])
+        else:
+            return False
+
+    def get_length_of_sides(self):
+        for i in range(len(self.vectors)):
+            yield self.vectors[i].get_length()
--- lesson06/home_work/hw06_hard.py
+++ lesson06/home_work/hw06_hard.py
@@ -10,3 +10,89 @@
 # С использованием классов.
 # Реализуйте классы сотрудников так, чтобы на вход функции-конструктора
 # каждый работник получал строку из файла
+
+
+class Worker:
+
+    workers_regex = '(?P<FN>[А-Яа-я]+)[\s]+(?P<LN>[А-Яа-я]+)[\s]+(?P<S>[\d]+)[\s]+(?P<P>[А-Яа-я]+)[\s]+(?P<N>[\d]+)'
+
+    last_name = None
+    first_name = None
+    profession = None
+
+    salary = 0
+    time_norm = 0
+    time_worked = 0
+
+    def __init__(self, data_string):
+        self.__parse_data(data_string)
+
+    def __parse_data(self, data):
+        import re
+        match = re.match(self.workers_regex, data)
+        self.last_name = match.group('LN')
+        self.first_name = match.group('FN')
+        self.profession = match.group('P')
+        self.salary = int(match.group('S'))
+        self.time_norm = int(match.group('N'))
+
+    def __str__(self):
+
+        fio = f'{self.last_name} {self.first_name}'
+        prof = f'{self.profession} ({self.salary})'
+        work = f'Work: {self.time_worked}/{self.time_norm}'
+        total = f'Profit: {self.get_total_profit()}'
+
+        return f'{fio:30}{prof:25}{work:20}{total}'
+
+    def get_total_profit(self):
+
+        sal_for_hour = self.salary / self.time_norm
+        diff = self.time_worked - self.time_norm
+        if diff == 0:
+            bonus = 0
+        elif diff > 0:
+            bonus = diff * sal_for_hour * 2
+        else:
+            bonus = diff * sal_for_hour
+        return round(self.salary + bonus, 2)
+
+
+def parse_hours(data):
+    import re
+    hours_regex = '(?P<FN>[А-Яа-я]+)[\s]+(?P<LN>[А-Яа-я]+)[\s]+(?P<W>[\d]+)'
+
+    match = re.match(hours_regex, data)
+    if len(match.groups()) != 3:
+        return None
+
+    return match.group('LN'), match.group('FN'), match.group('W')
+
+
+def parse_files(workers_file, hours_file):
+
+    workers = []
+    with open(workers_file, 'r', encoding='utf-8') as file:
+        for w in file.readlines()[1:]:
+            workers.append(Worker(w))
+    worked_list = []
+    with open(hours_file, 'r', encoding='utf-8') as file:
+        for h in file.readlines()[1:]:
+            parsed = parse_hours(h)
+            if len(parsed) != 3:
+                continue
+            worked_list.append(parsed)
+
+    for i in worked_list:
+        cur_worker = list(filter(lambda x: (x.last_name == i[0] and x.first_name == i[1]), workers))
+        if len(cur_worker) != 0:
+            cur_worker[0].time_worked = int(i[2])
+        pass
+
+    return workers
+
+
+if __name__ == '__main__':
+    workers = parse_files(workers_file='data//workers', hours_file='data//hours_of')
+    for w in workers:
+        print(w)
